ee0019872b1d0340d74801465b669ab6882c535f
rogercamps <rogercamps@me.com>
2017-04-12 17:29:27 -0700

    update routes. add, delete, update routes work correctly now

--- database/tasks.js
+++ database/tasks.js
@@ -5,7 +5,7 @@ const pgp = pgPromise()
 const db = pgp({ database: 'todolist' })
 
 const tasks = {
-  getAll: () => db.any('SELECT * FROM tasks ORDER by task_id'),
+  getAll: () => {console.log('entered getAll'); return db.any('SELECT * FROM tasks ORDER by task_id')},
   create: (note) => db.oneOrNone('INSERT INTO tasks (note) VALUES ($1) RETURNING task_id', [note]),
   update: (id, note) => db.one('UPDATE tasks SET note=$1 WHERE task_id=$2 RETURNING *', [id, note]),
   deleteTask: (id) => db.none('DELETE from tasks WHERE task_id=$1', [id])
