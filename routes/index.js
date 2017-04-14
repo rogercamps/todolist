669149b25d6bd8e43094e7e563387566ba6ed306
rogercamps <rogercamps@me.com>
2017-04-14 12:12:00 -0700

    finishes implementation of linter and removes bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,36 @@
 var express = require('express');
 var router = express.Router();
 const tasks = require('../database/tasks.js')
-const pgPromise = require('pg-promise')
+
 
 
 router.get('/', function(request, response) {
   tasks.getAll().then(tasks => {
-      response.render('index', {
-        title: 'To Do List',
-        tasks: tasks
-      })
+    response.render('index', {
+      title: 'To Do List',
+      tasks: tasks
     })
-    .catch(error => response.json(error))
+  })
+  .catch(error => response.json(error))
 })
 
 router.post('/createTask', function(request, response) {
   tasks.create(request.body.task).then(() => {
-      response.redirect('/')
-    })
-    .catch(error => response.json(error))
+    response.redirect('/')
+  })
+  .catch(error => response.json(error))
 })
 
 router.post('/deleteTask/:id', (request, response) => {
   tasks.deleteTask(request.params.id).then(() => {
-      response.redirect('/')
-    })
-    .catch(error => response.json(error))
+    response.redirect('/')
+  })
+  .catch(error => response.json(error))
 })
 
 router.get('/updateTask/:id', (request, response) => {
-  const { id } = request.params
 
-  Promise.all('SELECT * FROM tasks WHERE note_id=$1').then(data => {
+  Promise.all('SELECT * FROM tasks WHERE note_id=$1').then(() => {
     response.redirect('/')
   })
 })
@@ -39,16 +38,16 @@ router.get('/updateTask/:id', (request, response) => {
 
 router.post('/updateTask/:id', (request, response) => {
   tasks.update(request.body.task, request.params.id).then(() => {
-      response.redirect('/')
-    })
-    .catch(error => response.json(error))
+    response.redirect('/')
+  })
+  .catch(error => response.json(error))
 })
 
 router.post('/completeTask/:id', (request, response) => {
   tasks.completeTask(request.params.id).then(() => {
-      response.redirect('/')
-    })
-    .catch(error => response.json(error))
+    response.redirect('/')
+  })
+  .catch(error => response.json(error))
 })
 
 module.exports = router;
