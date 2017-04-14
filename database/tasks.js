c99bdee8f1f40d1ef4b46f360f8059c4d26b3c15
Keith Oppel <mistakeets@gmail.com>
2017-04-14 09:30:30 -0700

    WIP implementing complete button and update of naming convention

--- database/tasks.js
+++ database/tasks.js
@@ -5,10 +5,11 @@ const pgp = pgPromise()
 const db = pgp({ database: 'todolist' })
 
 const tasks = {
-  getAll: () => db.any('SELECT * FROM tasks ORDER by task_id'),
-  create: (note) => db.oneOrNone('INSERT INTO tasks (note) VALUES ($1) RETURNING task_id', [note]),
-  update: (id, note) => db.one('UPDATE tasks SET note=$1 WHERE task_id=$2 RETURNING *', [id, note]),
-  deleteTask: (id) => db.none('DELETE from tasks WHERE task_id=$1', [id])
+  getAll: () => db.any('SELECT * FROM tasks ORDER by note_id'),
+  create: (note) => db.oneOrNone('INSERT INTO tasks (note) VALUES ($1) RETURNING note_id', [note]),
+  update: (id, note) => db.one('UPDATE tasks SET note=$1 WHERE note_id=$2 RETURNING *', [id, note]),
+  deleteTask: (id) => db.none('DELETE from tasks WHERE note_id=$1', [id]),
+  completeTask: (id) => db.none('UPDATE tasks SET complete=true WHERE note_id=$1', [id])
 }
 
 module.exports = tasks;
